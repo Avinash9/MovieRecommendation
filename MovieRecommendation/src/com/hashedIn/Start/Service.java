7c3bac4041a8ec823c533ead434c1924f8299270
Avinash Garg <avinash@Avinash-HashedIn.(none)>
2012-12-22 23:13:45 +0530

    parser 1 and parser2 updated

---
+++ MovieRecommendation/src/com/hashedIn/Start/Service.java
@@ -0,0 +1,18 @@
+package com.hashedIn.Start;
+
+import com.hashedIn.Service.Manager;
+
+public class Service {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Manager m=new Manager();
+		//m.movieParsing();
+		m.userParsing();
+		//m.ratingParsing();
+	}
+
+}
